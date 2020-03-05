3ac041a1df4f0081a878400ba5be09353bf6b526
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-05 06:20:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,5 +8,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+<<<<<<< HEAD
   pp print_first_directors_movie_titles
+=======
+  
+  pp 
+>>>>>>> 66e55eceb3fc903122189e6dd8c67c60da6778d0
 end
